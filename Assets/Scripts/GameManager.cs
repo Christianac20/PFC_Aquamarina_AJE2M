e8f97a2a602357975c82fa2d272bd66a5a7509a8
RankeTheDev <christianac20@educastur.es>
2026-04-01 18:29:11 +0200

    Conservacion Objetos entre escenas
    Modificado metodo de movimiento de objetos entre escenas para permitir traspasar datos y objects entre escenas y prevenir duplicaciones

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,6 @@ public class GameManager : MonoBehaviour
 {
     #region VARIABLES
     public GameObject[] keepObjects;
-    public SceneTransition sceneTransition;
 
     #endregion
 
@@ -14,8 +13,6 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        sceneTransition = FindObjectOfType<SceneTransition>();
-
         for (int i = 0; i < keepObjects.Length; i++)
         {
             DontDestroyOnLoad(keepObjects[i]);
